211b5545e0f08d4e912c1a82288ca4fe49ee1263
Marcant Aimé <aime.marcant@hotmail.fr>
2024-12-03 22:30:12 +0000

    fix: uncomment tests of day 2

--- src/__tests__/day2.test.ts
+++ src/__tests__/day2.test.ts
@@ -24,23 +24,23 @@ describe("isThisSafe", () => {
 });
 
 describe("isThisSafeAllowOneError", () => {
-    // it("should return true for a valid sequence by skipping one level", () => {
-    //     expect(isThisSafeAllowOneError([1, 3, 2, 4, 5])).toBe(true);
-    //     expect(isThisSafeAllowOneError([8, 6, 4, 4, 1])).toBe(true);
-    // });
+    it("should return true for a valid sequence by skipping one level", () => {
+        expect(isThisSafeAllowOneError([1, 3, 2, 4, 5])).toBe(true);
+        expect(isThisSafeAllowOneError([8, 6, 4, 4, 1])).toBe(true);
+    });
 
     it("should return false for a sequence with more than one violation", () => {
         expect(isThisSafeAllowOneError([1, 2, 7, 8, 9])).toBe(false);
         expect(isThisSafeAllowOneError([9, 7, 6, 2, 1])).toBe(false);
     });
 
-    // it("should return true for a monotonic sequence within range without skipping", () => {
-    //     expect(isThisSafeAllowOneError([7, 6, 4, 2, 1])).toBe(true);
-    //     expect(isThisSafeAllowOneError([1, 3, 6, 7, 9])).toBe(true);
-    // });
+    it("should return true for a monotonic sequence within range without skipping", () => {
+        expect(isThisSafeAllowOneError([7, 6, 4, 2, 1])).toBe(true);
+        expect(isThisSafeAllowOneError([1, 3, 6, 7, 9])).toBe(true);
+    });
 
-    // it("should return true for an empty list or a single element", () => {
-    //     expect(isThisSafeAllowOneError([])).toBe(true);
-    //     expect(isThisSafeAllowOneError([22])).toBe(true);
-    // });
+    it("should return true for an empty list or a single element", () => {
+        expect(isThisSafeAllowOneError([])).toBe(true);
+        expect(isThisSafeAllowOneError([22])).toBe(true);
+    });
 });
